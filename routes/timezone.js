fc401b807727f86cd916aa6690f0fc797ded0d11
Woynshet Bilihatu <wbilihatu@gmail.com>
2021-12-20 11:57:30 +0300

    timezone routing app is added

---
+++ routes/timezone.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const request = require("request");
+const router = express.Router();
+
+router.get("/timezone/:city", async (req, res) => {
+  const city = req.params.city;
+  console.log(city);
+  // http://api.weatherapi.com/v1/timezone.json?key=73af8f29e27149e7b1a73152211812&q=Hawassa
+  var options = {
+    method: "GET",
+    url:
+      "http://api.weatherapi.com/v1/timezone.json?key=73af8f29e27149e7b1a73152211812&q=" +
+      city,
+    headers: {},
+  };
+
+  request(options, function (err, response) {
+    if (err) throw new Error(error);
+    console.log(response.body);
+    res.status(200).json(response.body);
+  });
+});
+
+module.exports = router;

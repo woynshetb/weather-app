c70c163aee720ba59c3874316e77bee6dc7be266
Woynshet Bilihatu <wbilihatu@gmail.com>
2021-12-20 11:18:26 +0300

    getting weather by city name is added

--- routes/weather.js
+++ routes/weather.js
@@ -5,10 +5,15 @@ const request = require("request");
 
 const router = express.Router();
 
-router.get("/fetching", async (req, res) => {
+router.get("/fetching/:city", async (req, res) => {
+  const city = req.params.city;
+  console.log(city);
   var options = {
     method: "GET",
-    url: "http://api.weatherapi.com/v1/forecast.json?key=73af8f29e27149e7b1a73152211812&q=Hawassa&days=1&aqi=no&alerts=no#",
+    url:
+      "http://api.weatherapi.com/v1/forecast.json?key=73af8f29e27149e7b1a73152211812&q=" +
+      city +
+      "&days=1&aqi=no&alerts=no#",
     headers: {},
   };
   request(options, function (error, response) {

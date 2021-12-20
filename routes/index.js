0cfe46d6eb35a6887969eec16f3a5204b2fb918e
Woynshet Bilihatu <wbilihatu@gmail.com>
2021-12-20 11:58:10 +0300

    new endpoint is added

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const router = require("express").Router();
 
 const weatherRouter = require("./weather");
 const astronomyRouter = require("./astronomy");
+const timezoneRouter = require("./timezone");
 
+router.use(timezoneRouter);
 router.use(astronomyRouter);
 router.use(weatherRouter);
 

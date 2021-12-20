061755b4323e8dae3d4c3c4a9e2e415a6b1b069d
Woynshet Bilihatu <wbilihatu@gmail.com>
2021-12-20 11:40:45 +0300

    astronomy end point for city is added

--- routes/astronomy.js
+++ routes/astronomy.js
@@ -1,18 +1,16 @@
 const express = require("express");
-
 const request = require("request");
-
 const router = express.Router();
 
-router.get("/fetching/:city", async (req, res) => {
+router.get("/astronomy/:city", async (req, res) => {
   const city = req.params.city;
   console.log(city);
   var options = {
     method: "GET",
     url:
-      "http://api.weatherapi.com/v1/forecast.json?key=73af8f29e27149e7b1a73152211812&q=" +
+      "http://api.weatherapi.com/v1/astronomy.json?key=73af8f29e27149e7b1a73152211812&q=" +
       city +
-      "&days=1&aqi=no&alerts=no#",
+      "&dt=2021-12-20",
     headers: {},
   };
   request(options, function (error, response) {
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 
 const weatherRouter = require("./weather");
+const astronomyRouter = require("./astronomy");
+
+router.use(astronomyRouter);
 router.use(weatherRouter);
 
 module.exports = router;
